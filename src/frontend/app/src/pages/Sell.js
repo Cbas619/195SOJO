30d52cc90af2f0492dc122f502e7b1958afd8a3d
TyrellSJSU <82182536+TyrellSJSU@users.noreply.github.com>
2023-01-26 16:57:58 -0800

    adding Sell page file

---
+++ src/frontend/app/src/pages/Sell.js
@@ -0,0 +1,14 @@
+import { MainNav } from "../components/Main/MainNav";
+import { MainCategories } from "../components/Main/MainCategories";
+
+export function Sell() {
+    return (
+        <>
+        <MainNav />
+        <MainCategories/>
+        <div className="background-1">
+        
+        </div>
+        </>
+    );
+}
