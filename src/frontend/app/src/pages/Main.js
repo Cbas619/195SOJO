268f44288d5166d5e9f244671d8ae9c45e84ce32
TyrellSJSU <82182536+TyrellSJSU@users.noreply.github.com>
2023-01-26 16:46:51 -0800

    updated css class names

--- src/frontend/app/src/pages/Main.js
+++ src/frontend/app/src/pages/Main.js
@@ -7,24 +7,25 @@ import { MainItemCards } from "../components/Main/MainItemCards";
 export function Main() {
   return (
     <>
+    <MainNav/> 
+    <MainCategories/>
     <div className="background-1">
-      <MainNav/> 
-      <MainCategories/>
       
       <div>
         <MainHeaders categoryHeader="Books for the brain | " categoryLink="/books"/>
-        <div className="mainItem">
+        <div className="mainItemCard">
           <MainItemCards itemName="Math book" itemPrice="12" itemImage="/images/placeholder.png"/>
         </div>
       </div>
 
       <div>
         <MainHeaders categoryHeader="Work with Supplies | " categoryLink="/officesupplies" />
-        <div className="mainItem">
+        <div className="mainItemCard">
           <MainItemCards itemName="Pencils" itemPrice="2" itemImage="/images/placeholder.png"/>
         </div>
-        
       </div>
+
+      
 
 
       <br></br>
