1aed4896066acf28eea3fa040701e8d50cd0db1f
TyrellSJSU <82182536+TyrellSJSU@users.noreply.github.com>
2023-01-26 16:52:29 -0800

    Updated Links

--- src/frontend/app/src/components/Home/HomeNav.js
+++ src/frontend/app/src/components/Home/HomeNav.js
@@ -2,22 +2,23 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import '../../App.scss'
+import { Link } from 'react-router-dom';
 
 export function HomeNav() {
   return (
     <Navbar bg="secondary" expand="lg">
       <Container>
-        <Navbar.Brand href="#home"><img className="logo" src="/images/logo.png"/></Navbar.Brand>
+        <Navbar.Brand><Link to="/"><img className="logo" src="/images/logo.png"/></Link></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
           </Nav>
           <Nav>
             <Nav.Link>
-              <div className='loginNav'>Login</div>
+              <Link to="/login"><div className='loginNav'>Login</div></Link>
             </Nav.Link>
             <Nav.Link>
-            <div className='signUpNav'>Sign Up</div>
+            <Link to="/signup"><div className='signUpNav'>Sign Up</div></Link>
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
