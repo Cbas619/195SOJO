9a0414612fef94334821880e6e52de24d65020b8
FG9732 <fernando.gonz9732@gmail.com>
2023-01-31 21:43:06 -0800

    Finished Login/Signup
    Finished Login/Signup

--- src/frontend/app/src/components/Login/LoginForm.js
+++ src/frontend/app/src/components/Login/LoginForm.js
@@ -3,8 +3,29 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col'
+import axios from 'axios'
+import { useState } from "react"
+import { useNavigate } from 'react-router-dom'
 
 export function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const login = async (e) => {
+    e.preventDefault(); 
+    try {
+      axios.post("http://localhost:4000/api/auth/login", {
+      email: email,
+      password: password,
+    }).then((response, err) => {
+      console.log(response);
+      navigate('/main');
+      
+    })} catch (error){
+      console.log(JSON.stringify(error));
+    }
+  };
   return (
     <Row>
       <Col>
@@ -12,18 +33,18 @@ export function LoginForm() {
 
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control type="email" placeholder="Enter email" onChange={(e) => {setEmail(e.target.value)}}/>
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control type="password" placeholder="Password" onChange={(e) => {setPassword(e.target.value)}}/>
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicCheckbox">
         <Form.Check type="checkbox" label="Check me out" />
       </Form.Group>
 
-      <Button variant="primary" type="submit">
+      <Button variant="primary" type="submit" onClick={login}>
         Submit
       </Button>
     </Form>
--- src/frontend/app/src/components/Signup/SignupForm.js
+++ src/frontend/app/src/components/Signup/SignupForm.js
@@ -3,37 +3,64 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col'
+import axios from 'axios'
+import { useState } from "react"
+import { useNavigate } from 'react-router-dom'
 
 export function SignupForm() {
+  
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const register = async (e) => {
+    e.preventDefault(); 
+    try {
+      axios.post("http://localhost:4000/api/auth/register", {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: password,
+    }).then((response, err) => {
+      console.log(response);
+      navigate('/login');
+      
+    })} catch (error){
+      console.log(JSON.stringify(error));
+    }
+  };
+  
   return (
     <Row>
       <Col>
     <Form className='container'>
         <Form.Group className="mb-3" controlId="formFirstName">
         <Form.Label>First Name</Form.Label>
-        <Form.Control type="firstName" placeholder="Enter First Name" />
+        <Form.Control type="firstName" placeholder="Enter First Name" onChange={(e) => {setFirstName(e.target.value)}}/>
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formLastName">
         <Form.Label>Last Name</Form.Label>
-        <Form.Control type="lastName" placeholder="Enter Last Name" />
+        <Form.Control type="lastName" placeholder="Enter Last Name" onChange={(e) => {setLastName(e.target.value)}}/>
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control type="email" placeholder="Enter email" onChange={(e) => {setEmail(e.target.value)}}/>
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control type="password" placeholder="Password" onChange={(e) => {setPassword(e.target.value)}}/>
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicCheckbox">
         <Form.Check type="checkbox" label="Check me out" />
       </Form.Group>
 
 
-      <Button variant="primary" type="submit">
+      <Button variant="primary" type="submit" onClick={register}>
         Submit
       </Button>
     </Form>
