874f5f94e3118c9ce2599afc1f9177ee022b9d76
TyrellSJSU <82182536+TyrellSJSU@users.noreply.github.com>
2023-02-24 14:39:53 -0800

    Updated main page
    "Your messages' and 'Featured items'

--- src/frontend/app/src/pages/Main.js
+++ src/frontend/app/src/pages/Main.js
@@ -9,22 +9,67 @@ export function Main() {
     <>
     <MainNav/> 
     <MainCategories/>
+
     <div className="background-1">
-      
-      <div>
-        <MainHeaders categoryHeader="Books for the brain | " categoryLink="/books"/>
-        <div className="mainItemCard">
-          <MainItemCards itemName="Math book" itemPrice="12" itemImage="/images/placeholder.png"/>
-        </div>
-        
+
+      <div className="mainMessagesSection">
+        <div id="mainMessagesHeader">Your messages</div>
+        <div className="mainLine-1"></div>
+        <div className="mainItemCardContainer">
+            <div className="mainItemCard">
+            <MainItemCards itemName="Math book" itemPrice="12" itemImage="/images/placeholder.png"/>
+            </div>
+            <div className="mainItemCard">
+            <MainItemCards itemName="Math book" itemPrice="12" itemImage="/images/placeholder.png"/>
+            </div>
+          </div>
       </div>
 
-      <div>
-        <MainHeaders categoryHeader="Work with Supplies | " categoryLink="/officesupplies" />
-        <div className="mainItemCard">
-          <MainItemCards itemName="Pencils" itemPrice="2" itemImage="/images/placeholder.png"/>
+      <div className="mainFeaturedSection">
+        <div id="mainPageHeader">Featured Items</div>
+        <div className="mainLine-1"></div>
+        <div className="mainFeaturedCategoryContainer">
+          <MainHeaders categoryHeader="Books for the brain | " linkHeader="See all books" categoryLink="/books"/>
+          <div className="mainItemCardContainer">
+            <div className="mainItemCard">
+            <MainItemCards itemName="Math book" itemPrice="12" itemImage="/images/placeholder.png"/>
+            </div>
+            <div className="mainItemCard">
+            <MainItemCards itemName="Math book" itemPrice="12" itemImage="/images/placeholder.png"/>
+            </div>
+            <div className="mainItemCard">
+            <MainItemCards itemName="Math book" itemPrice="12" itemImage="/images/placeholder.png"/>
+            </div>
+            <div className="mainItemCard">
+            <MainItemCards itemName="Math book" itemPrice="12" itemImage="/images/placeholder.png"/>
+            </div>
+            <div className="mainItemCard">
+            <MainItemCards itemName="Math book" itemPrice="12" itemImage="/images/placeholder.png"/>
+            </div>
+          </div>
+        </div>
+
+        <div className="mainFeaturedCategoryContainer">
+          <MainHeaders categoryHeader="Work with Supplies | " linkHeader="See all school supplies" categoryLink="/officesupplies" />
+          <div className="mainItemCardContainer">
+            <div className="mainItemCard">
+            <MainItemCards itemName="Pencils" itemPrice="2" itemImage="/images/placeholder.png"/>
+            </div>
+            <div className="mainItemCard">
+            <MainItemCards itemName="Pencils" itemPrice="2" itemImage="/images/placeholder.png"/>
+            </div>
+            <div className="mainItemCard">
+            <MainItemCards itemName="Pencils" itemPrice="2" itemImage="/images/placeholder.png"/>
+            </div>
+            <div className="mainItemCard">
+            <MainItemCards itemName="Pencils" itemPrice="2" itemImage="/images/placeholder.png"/>
+            </div>
+            
+          </div>
+          
         </div>
       </div>
+      
       <br></br>
     </div>
     
