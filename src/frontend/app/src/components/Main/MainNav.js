8d242382fd45e8f75f15902fe1e050a89a8a43dc
Khuong Huynh <huynhkhuong69>
2023-05-10 15:53:49 -0700

    add profile link to redirect to /account

--- src/frontend/app/src/components/Main/MainNav.js
+++ src/frontend/app/src/components/Main/MainNav.js
@@ -1,37 +1,71 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import '../../App.scss'
-import { Link } from 'react-router-dom';
-import { useSignOut } from 'react-auth-kit'
-import './SearchBar';
-import { SearchBar } from './SearchBar';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import "../../App.scss";
+import { Link } from "react-router-dom";
+import { useSignOut } from "react-auth-kit";
+import "./SearchBar";
+import { SearchBar } from "./SearchBar";
 
 export function MainNav() {
-  const signOut = useSignOut()
+  const signOut = useSignOut();
   return (
     <Navbar bg="secondary" expand="lg">
       <Container>
-        <Navbar.Brand><Link to ="/main"><img className="logo" src="/images/logo.png"/></Link></Navbar.Brand>
+        <Navbar.Brand>
+          <Link to="/main">
+            <img className="logo" src="/images/logo.png" />
+          </Link>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-          </Nav>
+          <Nav className="me-auto"></Nav>
           <SearchBar />
           <Nav>
             <Nav.Link>
-                <Link to ="/sell"><div className='mainNavLinks'>Sell</div></Link>
+              <Link to="/sell">
+                <div className="mainNavLinks">Sell</div>
+              </Link>
             </Nav.Link>
             <Nav.Link>
-            <Link to ="/chat"><div className='mainNavLinks'>Messages</div></Link>
+              <Link to="/chat">
+                <div className="mainNavLinks">Messages</div>
+              </Link>
             </Nav.Link>
-            <NavDropdown className='mainNavLinks' title="Account" id="basic-nav-dropdown">
-              <NavDropdown.Item><Nav.Link><Link to ="/edit"><div className='mainNavLinks'>Edit</div></Link></Nav.Link></NavDropdown.Item>
-              <NavDropdown.Item><Nav.Link><Link to ="/orders"><div className='mainNavLinks'>Orders</div></Link></Nav.Link></NavDropdown.Item>
-              </NavDropdown>
+            <NavDropdown
+              className="mainNavLinks"
+              title="Account"
+              id="basic-nav-dropdown"
+            >
+              <NavDropdown.Item>
+                <Nav.Link>
+                  <Link to="/account">
+                    <div className="mainNavLinks">Profile</div>
+                  </Link>
+                </Nav.Link>
+              </NavDropdown.Item>
+              <NavDropdown.Item>
+                <Nav.Link>
+                  <Link to="/edit">
+                    <div className="mainNavLinks">Edit</div>
+                  </Link>
+                </Nav.Link>
+              </NavDropdown.Item>
+              <NavDropdown.Item>
+                <Nav.Link>
+                  <Link to="/orders">
+                    <div className="mainNavLinks">Orders</div>
+                  </Link>
+                </Nav.Link>
+              </NavDropdown.Item>
+            </NavDropdown>
             <Nav.Link>
-            <Link to ="/"><div className='mainNavLinks' onClick={() => signOut()}>Sign Out</div></Link>
+              <Link to="/">
+                <div className="mainNavLinks" onClick={() => signOut()}>
+                  Sign Out
+                </div>
+              </Link>
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
